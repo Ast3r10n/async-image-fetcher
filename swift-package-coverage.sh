6d8e34541669a798641e0aec2ff92ac1c65b9d7c
Andrea Sacerdoti <andrea.sacerdoti@mobilesoft.it>
2021-03-19 18:18:57 +0100

    Set test.yml derived data path

--- swift-package-coverage.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-TEST_COV_REPORT_FULL_PATH=`find $1 -name '*.xcresult' | sort -r | head -1`
-TOTAL_XCTEST_COVERAGE=`xcrun xccov view --report $TEST_COV_REPORT_FULL_PATH | grep '.xctest' | head -1 | perl -pe 's/.+?(\d+\.\d+%).+/\1/'`
-echo "Test Coverage=$TOTAL_XCTEST_COVERAGE"
